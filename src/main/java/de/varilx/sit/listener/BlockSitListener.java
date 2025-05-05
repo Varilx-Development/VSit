cc832c2d74cfb91f9fbfcb26b880c78b6bca6126
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2025-05-05 08:59:57 -0500

    add logic
    I also realized I could reuse the 'block' variable again to clean the code up a bit

--- src/main/java/de/varilx/sit/listener/BlockSitListener.java
+++ src/main/java/de/varilx/sit/listener/BlockSitListener.java
@@ -29,15 +29,18 @@ public class BlockSitListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getClickedBlock() == null) return;
+        Block block = event.getClickedBlock();
+        if (block == null) return;
         VaxConfiguration configuration = BaseAPI.get().getConfiguration();
         if (!configuration.getBoolean("blocks.enabled")) return;
         if (!configuration.getBoolean("enabled")) return;
-        
-        Block block = event.getClickedBlock();
         if (configuration.getStringList("blocks.blocked-worlds").contains(block.getWorld().getName())) return;
         Player player = event.getPlayer();
         if (player.isSneaking()) return;
+        if (configuration.getBoolean("blocks.require-empty-hand")) {
+            Material mat = player.getItemInHand().getType();
+            if (mat != Material.AIR) return;
+        }
         
         for (String blockStr : configuration.getStringList("blocks.blocks")) {
             if (block.getType().name().toLowerCase().contains(blockStr.toLowerCase())) {
