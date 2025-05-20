fa28294f2840f4ca7f0d2a31a6a1c6b70026ab9d
Shadow <nicolafabian75@gmail.com>
2025-05-20 16:53:34 +0200

    Refactor PlayerSitListener to use PersistentDataContainer.
    Replaced metadata with PersistentDataContainer for better data management and reduced reliance on older APIs. Introduced a constructor to pass the plugin instance, enabling the use of NamespacedKey for identifying custom data tags. This improves code clarity and future scalability.

--- src/main/java/de/varilx/sit/listener/PlayerSitListener.java
+++ src/main/java/de/varilx/sit/listener/PlayerSitListener.java
@@ -2,20 +2,27 @@ package de.varilx.sit.listener;
 
 import de.varilx.BaseAPI;
 import de.varilx.configuration.VaxConfiguration;
+import de.varilx.sit.VSit;
+import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.persistence.PersistentDataType;
 
 public class PlayerSitListener implements Listener {
 
+    private final VSit plugin;
+
+    public PlayerSitListener(VSit plugin) {
+        this.plugin = plugin;
+    }
 
     @EventHandler
     public void onPlayerInteractAtEntity(PlayerInteractEntityEvent event) {
         VaxConfiguration configuration = BaseAPI.get().getConfiguration();
-        if(event.getPlayer().hasMetadata("vsit_blocked")) return;
-        if (!configuration.getBoolean("players.enabled")) return;
+        if(event.getPlayer().getPersistentDataContainer().has(new NamespacedKey(plugin, "vsit_blocked"), PersistentDataType.BOOLEAN)) return;
         if (!configuration.getBoolean("enabled")) return;
         if (configuration.getStringList("players.blocked-worlds").contains(event.getPlayer().getWorld().getName())) return;
         if (event.getPlayer().isSneaking()) return;
